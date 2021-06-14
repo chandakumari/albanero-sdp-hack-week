74dcba5f23df8683b3c1685b0517a39ae822ea32
chandakumari <chandakumari07071999@gmail.com>
2021-06-14 20:49:35 +0530

    Program1.cpp

--- Program1.cpp
+++ Program1.cpp
@@ -1,23 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-string GenerateTheString(int n){
-	string str ="";
-        if(n%2){
-            while(n-- > 0){
-                str += "a";
-            }
-        } 
-        else{
-            while(--n > 0){
-                str += "a";
-            } str += "b";
+string GenerateString(int n)
+{
+    string str = "";
+    if (n % 2)
+    {
+        while (n-- > 0)
+        {
+            str += "a";
         }
-        return str;
+    }
+    else
+    {
+        while (--n > 0)
+        {
+            str += "a";
+        }
+        str += "b";
+    }
+    return str;
 }
-int main(){
-	int n;
-	cin>>n;
-	string ans = GenerateTheString(n);
-	cout<<ans<<endl;
-	return 0;
+int main()
+{
+    int n;
+    cin >> n;
+    string ans = GenerateString(n);
+    cout << ans << endl;
+    return 0;
 }
